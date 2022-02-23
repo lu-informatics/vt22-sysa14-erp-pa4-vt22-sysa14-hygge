93e269e687f8609b5b15e2a853500f8379c202b4
karinengdal <karinlisa.engdal@gmail.com>
2022-02-23 14:29:35 +0000

    tried running the program (successful)

--- Assignment4Client2/src/Application.java
+++ Assignment4Client2/src/Application.java
@@ -1,9 +1,27 @@
+import org.tempuri.CronusServiceSoap;
+import org.tempuri.CronusServiceSoapProxy;
+
+import java.rmi.RemoteException;
+
+import org.tempuri.CRONUS_Sverige_AB_Employee; 
 
 public class Application {
 
 	public static void main(String[] args) {
-		System.out.println("Test");  
-
+		CronusServiceSoap proxy = new CronusServiceSoapProxy(); 
+		
+		try {
+			CRONUS_Sverige_AB_Employee[] employees = proxy.getEmployees();
+			
+			for(CRONUS_Sverige_AB_Employee employee : employees)  {
+				System.out.println(employee.getFirst_Name()); 
+			
+			}
+				
+		} catch (RemoteException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}  
 	}
 
 }
