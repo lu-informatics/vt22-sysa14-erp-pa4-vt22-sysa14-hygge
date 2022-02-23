08a2e50b5a543a0c597875262c64dd03f299d693
karinengdal <karinlisa.engdal@gmail.com>
2022-02-23 13:01:46 +0000

    Updated DAL

--- CronusWebApplication/CronusWebApplication/DataAccessLayer.cs
+++ CronusWebApplication/CronusWebApplication/DataAccessLayer.cs
@@ -7,5 +7,16 @@ namespace CronusWebApplication
 {
     public class DataAccessLayer
     {
+
+        public List<CRONUS_Sverige_AB_Employee> GetEmployees() 
+        {
+
+            using (CronusEntities cronusEntities = new CronusEntities()) 
+            { 
+                List<CRONUS_Sverige_AB_Employee> employees = cronusEntities.CRONUS_Sverige_AB_Employee.ToList();
+                return employees; 
+               
+            }
+        }
     }
 }
