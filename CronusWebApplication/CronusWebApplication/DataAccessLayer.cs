9e934e64f9fb134fb8d21cb3a7b7361675aa4b60
karinengdal <karinlisa.engdal@gmail.com>
2022-02-24 13:30:45 +0000

    Created an Add-method for Employees

--- CronusWebApplication/CronusWebApplication/DataAccessLayer.cs
+++ CronusWebApplication/CronusWebApplication/DataAccessLayer.cs
@@ -18,5 +18,19 @@ namespace CronusWebApplication
                
             }
         }
+
+        public void AddEmployee(string First_Name, string Last_Name, string Job_Title)
+        { 
+            using(CronusEntities cronusEntities =new CronusEntities())
+            {
+                CRONUS_Sverige_AB_Employee tmpEmployee = new CRONUS_Sverige_AB_Employee() { First_Name = First_Name, Last_Name = Last_Name, Job_Title = Job_Title }; 
+
+               cronusEntities.CRONUS_Sverige_AB_Employee.Add(tmpEmployee);
+
+               cronusEntities.SaveChanges();
+
+            }   
+                    
+               }
     }
 }
