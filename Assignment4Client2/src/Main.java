4106abe44913552c24649e37080c211ede7d0a64
karinengdal <karinlisa.engdal@gmail.com>
2022-03-01 10:33:10 +0000

    Created the Main

---
+++ Assignment4Client2/src/Main.java
@@ -0,0 +1,26 @@
+import java.rmi.RemoteException;
+
+import org.tempuri.CRONUS_Sverige_AB_Employee;
+import org.tempuri.CronusServiceSoap;
+import org.tempuri.CronusServiceSoapProxy;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		CronusServiceSoap proxy = new CronusServiceSoapProxy(); 
+		
+		try {
+			CRONUS_Sverige_AB_Employee[] employees = proxy.getEmployees();
+			
+			for(CRONUS_Sverige_AB_Employee employee : employees) {
+				
+				
+			}
+			
+		} catch (RemoteException e) {			
+			e.printStackTrace();
+		}   
+	}
+
+}
