7326b244ea382fadb6c6a603920ec2eeb39125dc
karinengdal <karinlisa.engdal@gmail.com>
2022-03-02 00:23:33 +0000

    Added comments in Main

--- Assignment4Client2/src/Main.java
+++ Assignment4Client2/src/Main.java
@@ -1,10 +1,10 @@
 
-
 public class Main {
 
 	public static void main(String[] args) {
-		EmployeeFrame employeeFrame = new EmployeeFrame();
-		Controller controller = new Controller(employeeFrame);
+		EmployeeFrame employeeFrame = new EmployeeFrame();			//Create an EmployeeFrame object. Since all the code is already in the constructor 
+																	//.. this will be enough to start the GUI
+		Controller controller = new Controller(employeeFrame);		//Create a controller to initialize the actionListeners
 	}
 
 }
